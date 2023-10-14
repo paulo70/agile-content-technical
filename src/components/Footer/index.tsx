4f38072e35e48928f476b67f80f46b2e3445bb76
paulo70 <paulofrontend@gmail.com>
2023-10-14 17:56:36 -0300

    add icon to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,10 @@
 import { Footer } from "./style"
+import {AiOutlineCopyright} from "react-icons/ai";
 
 const MainFooter = () => {
   return (
     <Footer>
-      <h5>Google 2021</h5>
+      <h5><AiOutlineCopyright size={10} /> Google 2021</h5>
       <h6>version 0.1.0</h6>
     </Footer>
   )
